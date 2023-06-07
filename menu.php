56a354fb18fe0b980fd55cac122c20350ee689bf
Jafet Barquero <barquero2312@hotmail.com>
2023-06-06 20:01:11 -0600

    First Version

---
+++ menu.php
@@ -0,0 +1,134 @@
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="megakit,business,company,agency,multipurpose,modern,bootstrap4">
+    
+    <meta name="author" content="themefisher.com">
+  
+    <title> Investors Journal</title>
+  
+    <!-- bootstrap.min css -->
+    <link rel="stylesheet" href="plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Icon Font Css -->
+    <link rel="stylesheet" href="plugins/themify/css/themify-icons.css">
+    <link rel="stylesheet" href="plugins/fontawesome/css/all.css">
+    <link rel="stylesheet" href="plugins/magnific-popup/dist/magnific-popup.css">
+    <!-- Owl Carousel CSS -->
+    <link rel="stylesheet" href="plugins/slick-carousel/slick/slick.css">
+    <link rel="stylesheet" href="plugins/slick-carousel/slick/slick-theme.css">
+  
+    <!-- Main Stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+  
+  </head>
+  
+  <body>
+  
+  <!-- Header Start -->
+  <header class="navigation">
+    <div class="header-top ">
+      <div class="container">
+        <div class="row justify-content-between align-items-center">
+          <div class="col-lg-2 col-md-4">
+            <div class="header-top-socials text-center text-lg-left text-md-left">
+              <a href="https://www.facebook.com/themefisher" target="_blank"><i class="ti-facebook"></i></a>
+              <a href="https://twitter.com/themefisher" target="_blank"><i class="ti-twitter"></i></a>
+              <a href="https://github.com/themefisher/" target="_blank"><i class="ti-github"></i></a>
+            </div>
+          </div>
+          <div class="col-lg-10 col-md-8 text-center text-lg-right text-md-right">
+            <div class="header-top-info">
+              <a href="tel:+23-345-67890">Call Us : <span>+506 8411-4793</span></a>
+              <a href="mailto:support@gmail.com"><i class="fa fa-envelope mr-2"></i><span>investingDIJ@gmail.com</span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav class="navbar navbar-expand-lg  py-4" id="navbar">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <span>Investors </span>Journal
+        </a>
+  
+        <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample09"
+          aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="fa fa-bars"></span>
+        </button>
+  
+        <div class="collapse navbar-collapse text-center" id="navbarsExample09">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false">Información</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown03">
+                <li><a class="dropdown-item" href="about.php">¿Quiénes Somos?</a></li>
+                <li><a class="dropdown-item" href="pricing.php">Precios</a></li>
+              </ul>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="service.php">Servicios</a></li>
+            <!-- <li class="nav-item"><a class="nav-link" href="project.php">Portfolio</a></li> -->
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown05" data-toggle="dropdown" aria-haspopup="true"
+                aria-expanded="false">Herramientas</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown05">
+                <li><a class="dropdown-item" href="blog-grid.php">TradingView</a></li>
+                <li><a class="dropdown-item" href="blog-sidebar.php">MetaTrader 4 / MetaTrader 5</a></li>
+  
+                <li><a class="dropdown-item" href="blog-single.php">Investing.com</a></li>
+                <li><a class="dropdown-item" href="blog-single.php">Libros Recomendados</a></li>
+              </ul>
+            </li>
+            <li class="nav-item"><a class="nav-link" href="contact.php">Contactenos</a></li>
+          </ul>
+  
+          <form class="form-lg-inline my-2 my-md-0 ml-lg-4 text-center">
+            <a href="contact.php" class="btn btn-solid-border btn-round-full">Iniciar sesión</a>
+          </form>
+        </div>
+      </div>
+    </nav>
+
+    <div class="tradingview-widget-container" style="width: 100%; height: 44px;">
+  <iframe scrolling="no" allowtransparency="true" frameborder="0" src="https://s.tradingview.com/embed-widget/ticker-tape/?locale=es#%7B%22symbols%22%3A%5B%7B%22proName%22%3A%22FX_IDC%3AEURUSD%22%2C%22title%22%3A%22EUR%2FUSD%22%7D%2C%7B%22proName%22%3A%22BITSTAMP%3ABTCUSD%22%2C%22title%22%3A%22BTC%2FUSD%22%7D%2C%7B%22proName%22%3A%22BITSTAMP%3AETHUSD%22%2C%22title%22%3A%22ETH%2FUSD%22%7D%2C%7B%22description%22%3A%22AUD%2FUSD%22%2C%22proName%22%3A%22FX%3AAUDUSD%22%7D%2C%7B%22description%22%3A%22USD%2FJPY%22%2C%22proName%22%3A%22FX%3AUSDJPY%22%7D%2C%7B%22description%22%3A%22GBP%2FUSD%22%2C%22proName%22%3A%22FX%3AGBPUSD%22%7D%2C%7B%22description%22%3A%22EUR%2FGBP%22%2C%22proName%22%3A%22FX%3AEURGBP%22%7D%2C%7B%22description%22%3A%22RIPPLE%22%2C%22proName%22%3A%22BITFINEX%3AXRPUSD%22%7D%2C%7B%22description%22%3A%22USD%2FCAD%22%2C%22proName%22%3A%22FX%3AUSDCAD%22%7D%5D%2C%22colorTheme%22%3A%22dark%22%2C%22isTransparent%22%3Afalse%2C%22displayMode%22%3A%22adaptive%22%2C%22width%22%3A%22100%25%22%2C%22height%22%3A78%2C%22utm_source%22%3A%22smpprofit.com%22%2C%22utm_medium%22%3A%22widget_new%22%2C%22utm_campaign%22%3A%22ticker-tape%22%7D" style="box-sizing: border-box; height: 74px; width: 100%;"></iframe>
+  
+<style>
+	.tradingview-widget-copyright {
+		font-size: 13px !important;
+		line-height: 32px !important;
+		text-align: center !important;
+		vertical-align: middle !important;
+		font-family: 'Trebuchet MS', Arial, sans-serif !important;
+		color: #9db2bd !important;
+	}
+
+	.tradingview-widget-copyright .blue-text {
+		color: #2196f3 !important;
+	}
+
+	.tradingview-widget-copyright a {
+		text-decoration: none !important;
+		color: #9db2bd !important;
+	}
+
+	.tradingview-widget-copyright a:visited {
+		color: #9db2bd !important;
+	}
+
+	.tradingview-widget-copyright a:hover .blue-text {
+		color: #38acdb !important;
+	}
+
+	.tradingview-widget-copyright a:active .blue-text {
+		color: #299dcd !important;
+	}
+
+	.tradingview-widget-copyright a:visited .blue-text {
+		color: #2196f3 !important;
+	}
+	</style></div>
+  </header>
